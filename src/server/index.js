2e6af5ffe3f5e5755c881794e6d3845a653ab711
Machine[3R] <machine-3R@lanenga.net>
2020-12-30 17:37:19 +0100

    minimale server setup

---
+++ src/server/index.js
@@ -0,0 +1,17 @@
+const https = require('https');
+const express = require('express');
+const pem = require('pem');
+
+pem.createCertificate({days: 1, selfSigned: true}, function (err, keys) {
+    if (err) {
+        throw err;
+    }
+    var app = express();
+    app.use(express.static('public'));
+    app.use(express.static('public/views'));
+
+    https
+        .createServer({key: keys.serviceKey, cert: keys.certificate}, app)
+        .listen(443);
+});
+console.log('project: server started');
