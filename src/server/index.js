198f2aa232767c54e5ab405752628c0e4faf49c1
Machine[3R] <machine-3R@lanenga.net>
2020-12-31 20:57:54 +0100

    added socket.io

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,40 @@
+//process.env.DEBUG = 'express:*';
+const pem = require('pem');
+const path = require('path');
 const https = require('https');
 const express = require('express');
-const pem = require('pem');
+const socketio = require('socket.io');
+const expressWs = require('express-ws');
 
-pem.createCertificate({days: 1, selfSigned: true}, function (err, keys) {
-    if (err) {
-        throw err;
-    }
-    var app = express();
-    app.use(express.static('public'));
+pem
+    .createCertificate({days: 1, selfSigned: true}, async (err, keys) => {
+        if (err) {
+            throw err;
+        }
 
-    https
-        .createServer({key: keys.serviceKey, cert: keys.certificate}, app)
-        .listen(443);
-});
-console.log('project: server started');
+        let app = express();
+        app.use(express.static('public'));
+
+        let server = https
+            .createServer({
+                key: keys.serviceKey,
+                cert: keys.certificate
+            }, app)
+            .listen(443, data => {
+                console.log('Server started listening...');
+            });
+
+        let io = socketio(server);
+        io.on('connection', socket => {
+
+            socket
+                .on('hello', message => {
+                    console.log(message);
+                })
+                .on('disconnect', () => {
+                    console.log('client disconnected');
+                });
+
+            socket.emit('hi', 'Server says "hi".');
+        });
+    });
